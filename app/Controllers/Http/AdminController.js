512451f2040f2dfae76a3581ffcd3d131584579f
zaenur <zaenur.rochman98@gmail.com>
2020-03-03 23:01:52 +0700

    get admin data

--- app/Controllers/Http/AdminController.js
+++ app/Controllers/Http/AdminController.js
@@ -1,6 +1,13 @@
 'use strict'
 
 class AdminController {
+    async index({response}) {
+        let admin = await Admin.all();
+
+        return response.json({
+            message: admin
+        })
+    }
 }
 
 module.exports = AdminController
